b93800fb7c91dfc39a0e7913373226d9e4a6c639
Chilet-Okeke <okekechiletaram@gmail.com>
2024-08-04 16:45:38 +0100

    new fixes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
         booking_font: ["Work Sans", "sans-serif"],
         booking_font_bold: ["Work Sans", "sans-serif"],
         booking_font_normal: ["Work Sans", "sans-serif"],
-        booking_font4: ["Bebas Neue", "sans-serif"],
+        booking_font4: ["Work Sans", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
